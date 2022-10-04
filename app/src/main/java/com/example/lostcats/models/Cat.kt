8295f78a24f1c4fc5eb4d1a0c6d30fbdcd6901ff
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-10-04 10:55:09 +0200

    Setup constructor

--- app/src/main/java/com/example/lostcats/models/Cat.kt
+++ app/src/main/java/com/example/lostcats/models/Cat.kt
@@ -1,12 +1,18 @@
 package com.example.lostcats.models
 
 data class Cat (
-    var id: Int,
-    var name: String,
-    var description: String,
-    var place: String,
-    var reward: Int,
-    var userId: String,
-    var date: Long,
-    var pictureUrl: String
-)
+    val id: Int,
+    val name: String,
+    val description: String,
+    val place: String,
+    val reward: Int,
+    val userId: String,
+    val date: Long,
+    val pictureUrl: String) {
+    constructor(name: String, description: String, place: String, reward: Int, userId: String, pictureUrl: String):
+            this(-1,name,description,place,reward,userId,System.currentTimeMillis()/1000,pictureUrl)
+
+    override fun toString(): String{
+        return "$id $name $description $place $reward $userId $date $pictureUrl"
+    }
+}
